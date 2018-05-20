daf25f815ff0da31ccdb7ad1ec3cb5b485d56cb6
Luke Worley <luke.worley@colorado.edu>
2018-05-20 16:48:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,4 +12,4 @@ def display_board(board=[" ", " ", " ", " ", "X", " ", " ", " ", " "])
   end
 end
   
-display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+#display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
