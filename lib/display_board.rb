4c57606eaa3226abe43a4ae64d874e31ced72192
Luke Worley <luke.worley@colorado.edu>
2018-05-20 16:37:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 def display_board(board)
   0.step(3,3) do |i|
-    puts "#{board[i]}|#{board[i+1]}|#{board[i+2]}"
+    puts " #{board[i]} | #{board[i+1]} | #{board[i+2]}"
     puts "-----------"
   end
 end
