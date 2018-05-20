83698d7eb39625bc9777e63c9853645227091022
Luke Worley <luke.worley@colorado.edu>
2018-05-20 16:42:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
+def display_board(board=[" ", " ", " ", " ", "X", " ", " ", " ", " "])
   0.step(6,3) do |i|
     if (i<6)
       puts " #{board[i]} | #{board[i+1]} | #{board[i+2]}"
