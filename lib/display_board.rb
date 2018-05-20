765b49e34a593560ac5fdbc067abdfb0dab223c7
Luke Worley <luke.worley@colorado.edu>
2018-05-20 16:47:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,10 @@
 def display_board(board=[" ", " ", " ", " ", "X", " ", " ", " ", " "])
   0.step(6,3) do |i|
     if (i<6)
-      puts "#{board[i]} | #{board[i+1]} | #{board[i+2]}"
+      puts " #{board[i]} | #{board[i+1]} | #{board[i+2]} "
       puts "-----------"
     else
-      puts "#{board[i]} | #{board[i+1]} | #{board[i+2]}"
+      puts " #{board[i]} | #{board[i+1]} | #{board[i+2]} "
     end
   end
 end
